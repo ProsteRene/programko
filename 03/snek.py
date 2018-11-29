c85c257371c7e99e58f98875ff00d4d4d24a6336
ProsteRene <loksfd@seznam.cz>
2018-11-29 18:18:34 +0100

    vyroben šnek

---
+++ 03/snek.py
@@ -0,0 +1,41 @@
+from turtle import forward, left,right, exitonclick
+
+
+
+for x in range(71):
+	forward(x / 3)
+	right(12)
+
+left(131)
+forward(100)
+left(90)
+forward(70)
+left(90)
+
+for x in range(28):
+	right(12)
+	forward(3)
+   
+	
+left(65)
+forward(72)
+left(90)
+forward(50)
+
+for x in range(15):
+	forward(3)
+	right(12)
+
+forward(500)
+right(170)
+forward(210)
+	
+	
+
+	
+	
+
+	
+	
+	
+exitonclick()
